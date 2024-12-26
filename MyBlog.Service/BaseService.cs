4ec2fc5b8484c3f67c54204d3521eac2b2904b59
frankzhang <frank.zhang.sa.au@gmail.com>
2024-12-26 10:50:44 +1000

    In the service layer class: access methods through the interface _iBaseRepository;

--- MyBlog.Service/BaseService.cs
+++ MyBlog.Service/BaseService.cs
@@ -13,7 +13,7 @@ namespace MyBlog.Service
     public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : class, new()
     {
         //  222. service layer：  class implement interface;
-        //从 子类的构造函数 中传入
+        //从 子类的构造函数 中传入， 通过接口_iBaseRepository 来访问方法
         protected IBaseRepository<TEntity> _iBaseRepository;
         public async Task<bool> CreateAsync(TEntity entity)
         {
