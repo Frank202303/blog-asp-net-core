c63ab100cafb7d6ebf1e065e8d884fb8a7185a6d
frankzhang <frank.zhang.sa.au@gmail.com>
2024-12-26 10:37:09 +1000

    The subclass must first inherit the parent class, and then inherit the sub-interface;

--- MyBlog.Repository/TypeInfoRepository.cs
+++ MyBlog.Repository/TypeInfoRepository.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace MyBlog.Repository
 {
+    // the subclass must first inherit the parent class, and then inherit the sub-interface;
     //实现 ITypeInfoRepository接口
     public class TypeInfoRepository : BaseRepository<TypeInfo>, ITypeInfoRepository
     {
