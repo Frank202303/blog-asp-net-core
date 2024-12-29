00624e8b4be4c81918e136fc045cc24c3508ec78
frankzhang <frank.zhang.sa.au@gmail.com>
2024-12-29 15:06:39 +1000

    1. The MyBlog.JWT project is added to add a server for JWT to obtain the token code. 2. Config: IOC 3. Config: Startup, appsettings 4. run MyBlog.JWT project 5. in browser: login, try [http://localhost:6060/swagger/index.html] username: admintry userpwd: admintry So we can get jwtToken data: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW50cnkiLCJJZCI6IjEiLCJVc2VyTmFtZSI6ImFkbWludHJ5IiwibmJmIjoxNzM1NDQ4NTExLCJleHAiOjE3MzU0NTIxMTEsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NjA2MCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCJ9.15EISpfOIr9oMOS6acXpqpOj3340MKUKGl1vXZrn3AQ"

--- MyBlog.JWT/Controllers/AuthoizeController.cs
+++ MyBlog.JWT/Controllers/AuthoizeController.cs
@@ -1,5 +1,6 @@
-﻿
-
+﻿// 添加 MyBlog.JWT 项目是 为了
+//    添加一个服务器，用于JWT 获取 token码
+// 然后添加一个控制器： 授权 控制器 AuthoizeController
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -23,27 +24,36 @@ namespace MyBlog.JWT.Controllers
     public class AuthoizeController : ControllerBase
     {
         private readonly IWriterInfoService _iWriterInfoService;
+        // 依赖注入
         public   AuthoizeController(IWriterInfoService iWriterInfoService)
         {
             this._iWriterInfoService = iWriterInfoService;
         }
+
+        // 登录 方法
         [HttpPost("Login")]
         public async Task<ApiResult> Login(string username, string userpwd)
         {
             string pwd = MD5Helper.MD5Encrypt32(userpwd);//加密
             //数据 校验
-            var writer= await _iWriterInfoService.FindAsync(c=> c.UserName== username &&c.UserPwd==pwd);
-            if (writer!=null)
+
+            var writer = await _iWriterInfoService.FindAsync(c => c.UserName == username && c.UserPwd == pwd);
+            if (writer != null)
             {
                 //登录 成功
+                //  开始 copy 代码
                 var claims = new Claim[]
                 {
+                    // 3个键值对   key：value
+                    // 不能 放 敏感 信息
                     new Claim(ClaimTypes.Name,writer.Name), //相当于 身份证
                     new Claim("Id",writer.Id.ToString()),
-                     new Claim("UserName",writer.UserName)// 不能 放 敏感 信息
+                     new Claim("UserName",writer.UserName)
                 };
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SDMC-CJAS1-SAD-DFSFA-SADHJVF-VF"));// 密钥 最少16位
                 //issuer 代表 颁发 Token的 web 应用程序，audience 是Token的受理者
+                // 6060 在 launchSettings.json里设置
+                // 5000 在 MyBlog.WebApi的 launchSettings.json里设置
                 var token = new JwtSecurityToken(
                     issuer:"http://localhost:6060",
                     audience: "http://localhost:5000",
@@ -53,7 +63,9 @@ namespace MyBlog.JWT.Controllers
                     signingCredentials: new SigningCredentials(key,SecurityAlgorithms.HmacSha256)// key后 是逗号， 把密钥 用上
                     );
                 var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
-                return  ApiResultHelper.Success(jwtToken);// 返回 Token 值
+                //  结束 copy 代码
+
+                return ApiResultHelper.Success(jwtToken);// 返回 Token 值
             }
             else
             {
